1677d1180603d2f243965e4a3aa5ac5df75718d2
abhijeet <abhijeet.tate@gslab.com>
2021-07-16 09:10:05 +0530

    My First Commit

---
+++ Logging/src/main/java/com/package1/Logging/App.java
@@ -0,0 +1,13 @@
+package com.package1.Logging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
